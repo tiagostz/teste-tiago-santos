174486add2fc36715b000f2148070d974cf65996
tiagostz <tiagosantos.01016@gmail.com>
2025-04-03 15:38:09 -0300

    adicionado os servicos para gerenciar os equipamentos e estados

--- src/app/services/equipment/equipment.service.ts
+++ src/app/services/equipment/equipment.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Equipment } from '../models/equipment.model';
+import { EquipmentState, EquipmentStateHistory } from '../models/state.model';
+import { EquipmentPosition } from '../models/position.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class EquipmentService {
+  private dataPath = '/assets/data/';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getEquipments(): Observable<Equipment[]> {
+    return this.http.get<Equipment[]>(`${this.dataPath}equipment.json`);
+  }
+
+  getEquipmentStates(): Observable<EquipmentState[]> {
+    return this.http.get<EquipmentState[]>(`${this.dataPath}equipmentState.json`);
+  }
+
+  getEquipmentStateHistory(): Observable<EquipmentStateHistory[]> {
+    return this.http.get<EquipmentStateHistory[]>(`${this.dataPath}equipmentStateHistory.json`);
+  }
+
+  getEquipmentPositions(): Observable<EquipmentPosition[]> {
+    return this.http.get<EquipmentPosition[]>(`${this.dataPath}equipmentPositionHistory.json`);
+  }
 }
--- src/app/services/maps/maps.service.ts
+++ src/app/services/maps/maps.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import * as L from 'leaflet';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class MapsService {
+export class MapService {
+  private map!: L.Map;
 
-  constructor() { }
+  initializeMap(mapContainerId: string) {
+    this.map = L.map(mapContainerId).setView([-19.126536, -45.947756], 5);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© OpenStreetMap contributors',
+    }).addTo(this.map);
+  }
+
+  addEquipmentMarker(lat: number, lon: number, name: string, color: string) {
+    L.circleMarker([lat, lon], {
+      color,
+      radius: 10,
+      fillOpacity: 0.8,
+    })
+      .bindPopup(`<b>${name}</b>`)
+      .addTo(this.map);
+  }
 }
