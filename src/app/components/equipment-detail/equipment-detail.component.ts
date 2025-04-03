7f7081de62785e8e0e32c331f4aae201bc31bc8e
tiagostz <tiagosantos.01016@gmail.com>
2025-04-03 15:32:16 -0300

    criacao dos componentes que serao utilizados para listagem, equipamentos e historicos

---
+++ src/app/components/equipment-detail/equipment-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-equipment-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './equipment-detail.component.html',
+  styleUrl: './equipment-detail.component.scss'
+})
+export class EquipmentDetailComponent {
+
+}
