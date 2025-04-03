64136403a2aee70b4fbda7cecef4241523941fe9
tiagostz <tiagosantos.01016@gmail.com>
2025-04-03 11:58:32 -0300

    criacao do component equipment-history

---
+++ src/app/components/equipment-history/equipment-history.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-equipment-history',
+  standalone: true,
+  imports: [],
+  templateUrl: './equipment-history.component.html',
+  styleUrl: './equipment-history.component.scss'
+})
+export class EquipmentHistoryComponent {
+
+}
